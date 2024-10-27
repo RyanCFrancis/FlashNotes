36a8da8741c00f1dcd75871f3ff655e8ad16d226
maradC <maradiagachristopher5@gmail.com>
2024-10-27 07:45:33 -0400

    Created the Deck class and implemented attributes and methods from UML Diagram.

---
+++ src/main/java/org/flashnotes/flashnotes/Deck.java
@@ -0,0 +1,49 @@
+package org.flashnotes.flashnotes;
+
+public class Deck {
+
+    private Card[] cards;
+    private String[] sharedUsers;
+    private String ownerOfDeck;
+    private String nameOfDeck;
+    private String category;
+    private int id;
+
+
+    public Deck (String ownerOfDeck, String nameOfDeck, String category) {
+
+    }
+    public void addCard(String front, String back) {
+
+    }
+    public void removeCard(int index){
+
+    }
+    public Card[] getCards(){
+
+    }
+    public void setCategory(String category){
+
+    }
+    public void setNameOfDeck(String nameOfDeck){
+
+    }
+    public String getCategory(){
+
+    }
+    public String getOwnerOfDeck(){
+
+    }
+    public String getNameOfDeck(){
+
+    }
+    public String[] getSharedUsers(){
+
+    }
+    public void  addSharedUser(String username){
+
+    }
+
+
+
+}
