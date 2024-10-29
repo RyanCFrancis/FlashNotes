aec48dd1480ba92b48b6df7e093f3be03dbaee28
Charles Gonzalez <gonzc3@farmingdale.edu>
2024-10-29 12:26:12 -0400

    pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module org.flashnotes.flashnotes {
     requires javafx.controls;
     requires javafx.fxml;
+    requires google.cloud.firestore;
+    requires firebase.admin;
+    requires com.google.auth.oauth2;
+    requires com.google.auth;
 
 
     opens org.flashnotes.flashnotes to javafx.fxml;
     exports org.flashnotes.flashnotes;
+    exports org.flashnotes.flashnotes.Firebase;
+    opens org.flashnotes.flashnotes.Firebase to javafx.fxml;
 }
--- src/main/java/org/flashnotes/flashnotes/MainRunner.java
+++ src/main/java/org/flashnotes/flashnotes/MainRunner.java
@@ -4,12 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.flashnotes.flashnotes.Firebase.FireBaseActions;
 
 import java.io.IOException;
 
 public class MainRunner extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        FireBaseActions.init();
         FXMLLoader fxmlLoader = new FXMLLoader(MainRunner.class.getResource("SplashScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
