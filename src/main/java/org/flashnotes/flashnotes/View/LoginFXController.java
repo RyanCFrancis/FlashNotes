8a5feb62a14284c95855ada35a129835d05091bf
maradC <maradiagachristopher5@gmail.com>
2024-11-09 14:18:02 -0500

    Added the skeleton based off of the fxml and fixed my pom.xml file

--- src/main/java/org/flashnotes/flashnotes/View/LoginFXController.java
+++ src/main/java/org/flashnotes/flashnotes/View/LoginFXController.java
@@ -1,4 +1,30 @@
 package org.flashnotes.flashnotes.View;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginFXController {
+
+    @FXML
+    private Button RegisterButton;
+
+    @FXML
+    private Label flashLabel2;
+
+    @FXML
+    private PasswordField passwordTxt;
+
+    @FXML
+    private Label registerLabel;
+
+    @FXML
+    private Hyperlink uploadImageHyperLink;
+
+    @FXML
+    private TextField usernameTxt;
+
 }
