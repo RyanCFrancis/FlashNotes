4ba56110a8c3cd2ee71def3f8140f7bb810d816c
maradC <maradiagachristopher5@gmail.com>
2024-11-15 12:35:14 -0500

    added all the necessaries fx:id's to fxml files

---
+++ src/main/java/org/flashnotes/flashnotes/View/MainMenuFXController.java
@@ -0,0 +1,26 @@
+package org.flashnotes.flashnotes.View;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class MainMenuFXController {
+
+    @FXML
+    private Text FlashNotes;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Text home;
+
+    @FXML
+    private ImageView menuIcon;
+
+    @FXML
+    private VBox vBox;
+
+}
