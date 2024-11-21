f8665d9681c486c1d35bfdc8fbcbc7a3b7292686
Charles Gonzalez <gonzc3@farmingdale.edu>
2024-11-21 13:06:25 -0500

    first commit

--- src/main/java/org/flashnotes/flashnotes/View/MainRunner.java
+++ src/main/java/org/flashnotes/flashnotes/View/MainRunner.java
@@ -14,9 +14,10 @@ public class MainRunner extends Application {
     @Override
     public void start(Stage stage) throws IOException, FirebaseAuthException {
         FireBaseActions.init();
-        FXMLLoader fxmlLoader = new FXMLLoader(MainRunner.class.getResource("/org/flashnotes/flashnotes/SplashScreen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainRunner.class.getResource("/org/flashnotes/flashnotes/StudyScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("FlashNotes");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
