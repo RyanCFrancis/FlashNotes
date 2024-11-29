2ffb6e6c9023dd5f42fe920d27053067cedb6121
Ryan F <ryan8francis@gmail.com>
2024-11-28 21:56:01 -0500

    Made fxcontroller for makecard and made most of the fxml file, need to change the textfields to textareas. get feedback form the boys

---
+++ src/main/java/org/flashnotes/flashnotes/View/MakeCardFXController.java
@@ -0,0 +1,33 @@
+package org.flashnotes.flashnotes.View;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class MakeCardFXController {
+
+
+
+
+
+    //just for later implementation when event handlers are set up
+    @FXML
+    public void initialize() {
+
+    }
+
+
+    @FXML
+    private void createButton(){
+
+
+    }
+
+
+
+}
